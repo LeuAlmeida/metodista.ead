1dc2a3fd88f23509e16f906873a43e9844c343d6
Léu Almeida <leo@webid.net.br>
2019-11-10 01:39:04 -0300

    Update Footer

--- @plone/volto/src/components/theme/Footer/Footer.jsx
+++ @plone/volto/src/components/theme/Footer/Footer.jsx
@@ -6,19 +6,7 @@
 import React from 'react';
 import { Container, Divider, List, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import {
-  FormattedMessage,
-  defineMessages,
-  injectIntl,
-  intlShape,
-} from 'react-intl';
-
-const messages = defineMessages({
-  copyright: {
-    id: 'Copyright',
-    defaultMessage: 'Copyright',
-  },
-});
+import { FormattedMessage, injectIntl, intlShape } from 'react-intl';
 
 /**
  * Component to display the footer.
@@ -26,70 +14,13 @@ const messages = defineMessages({
  * @param {Object} intl Intl object
  * @returns {string} Markup of the component
  */
+
 const Footer = ({ intl }) => (
-  <Segment vertical padded inverted color="red" textAlign="center">
+  <Segment vertical padded inverted color="blue" textAlign="left">
     <Container>
-      <Segment basic inverted color="red" className="discreet">
-        <FormattedMessage
-          id="The {plonecms} is {copyright} 2000-{current_year} by the {plonefoundation} and friends."
-          defaultMessage="The {plonecms} is {copyright} 2000-{current_year} by the {plonefoundation} and friends."
-          values={{
-            plonecms: (
-              <FormattedMessage
-                id="Plone{reg} Open Source CMS/WCM"
-                defaultMessage="Plone{reg} Open Source CMS/WCM"
-                values={{ reg: <sup>®</sup> }}
-              />
-            ),
-            copyright: (
-              <abbr title={intl.formatMessage(messages.copyright)}>©</abbr>
-            ),
-            current_year: new Date().getFullYear(),
-            plonefoundation: (
-              <a className="item" href="http://plone.org/foundation">
-                <FormattedMessage
-                  id="Plone Foundation"
-                  defaultMessage="Plone Foundation"
-                />
-              </a>
-            ),
-          }}
-        />{' '}
-        <FormattedMessage
-          id="Distributed under the {license}."
-          defaultMessage="Distributed under the {license}."
-          values={{
-            license: (
-              <a
-                className="item"
-                href="http://creativecommons.org/licenses/GPL/2.0/"
-              >
-                <FormattedMessage
-                  id="GNU GPL license"
-                  defaultMessage="GNU GPL license"
-                />
-              </a>
-            ),
-          }}
-        />
-      </Segment>
       <List horizontal inverted>
         <List.Item href="/sitemap">
-          <FormattedMessage id="Site Map" defaultMessage="Site Map" />
-        </List.Item>
-        <List.Item href="/accessibility-info">
-          <FormattedMessage id="Accessibility" defaultMessage="Accessibility" />
-        </List.Item>
-        <List.Item>
-          <Link to="contact-form" className="item">
-            <FormattedMessage id="Contact" defaultMessage="Contact" />
-          </Link>
-        </List.Item>
-        <List.Item href="http://plone.com">
-          <FormattedMessage
-            id="Powered by Plone & Python"
-            defaultMessage="Powered by Plone & Python"
-          />
+          <FormattedMessage id="Site Map" defaultMessage="Educação Metodista" />
         </List.Item>
       </List>
     </Container>
